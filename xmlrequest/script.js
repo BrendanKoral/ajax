e78cfd78d6128b69d0facc6311e9b6c792ee544f
Brendan Koral <brendanhkoral@gmail.com>
2016-05-06 11:54:01 -0500

    Add completed XML request practice script

--- xmlrequest/script.js
+++ xmlrequest/script.js
@@ -1,3 +1,33 @@
 /**
  * Created by Koral on 5/4/2016.
  */
+
+var request;
+if (window.XMLHttpRequest) {
+    request = new XMLHttpRequest();
+} else {
+    ActiveXObject("Microsoft.XMLHTTP");
+}
+
+request.open('GET', 'data.xml');
+request.onreadystatechange = function () {
+    if ((request.readyState===4) && (request.status===200)) {
+
+        //Get the text from the first item using the node. This removes tag
+
+        console.log(request.responseXML.getElementsByTagName('name')[1].childNodes[0].nodeValue);
+        //can also use .firstChild. This now has the text without the quotes or tags
+
+        var items = request.responseXML.getElementsByTagName('name');
+
+        var output = '<ul>';
+
+        for (var i = 0; i < items.length; i++) {
+            output += '<li>' + items[i].firstChild.nodeValue + '</li>';
+        }
+        output += '</ul>';
+        document.getElementById('update').innerHTML = output;
+    }
+};
+
+request.send();
